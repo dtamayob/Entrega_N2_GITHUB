d432be6791d1ebc6cce296483d15b92e418dd075
dtamayob <valenblandon10@gmail.com>
2025-04-07 19:47:58 -0500

    Agregar archivos de proyecto.

---
+++ Entrega_N2_GITHUB/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Entrega_N2_GITHUB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Entrega_N2_GITHUB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
